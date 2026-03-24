314f6e70e990c30a5e9f3928ee8d4a8169186556
alexandreet92-lang <alexandre.et92@gmail.com>
2026-03-24 09:29:54 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { Card, StatCard } from '../components/ui/Card'
-import { Button } from '../components/ui/Button'
-import { Badge } from '../components/ui/Badge'
-import { cn, formatTime, getTSBColor, getReadinessLabel } from '../lib/utils'
+import { Card, StatCard } from '@/components/ui/Card'
+import { Button } from '@/components/ui/Button'
+import { Badge } from '@/components/ui/Badge'
+import { cn, formatTime, getTSBColor, getReadinessLabel } from '@/lib/utils'
 
 const LOAD = { ctl: 84, atl: 91, tsb: -7, volume: 12.4 }
 

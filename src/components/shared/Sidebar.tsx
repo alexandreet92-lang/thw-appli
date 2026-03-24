28ae541e08b82bd501477137eb60545c27c72c88
alexandreet92-lang <alexandre.et92@gmail.com>
2026-03-24 08:48:09 +0100

    Update and rename Sidebar.tsx to Sidebar.tsx

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -2,8 +2,8 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useTheme } from '@/src/hooks/useTheme'
-import { cn } from '@/src/lib/utils'
+import { useTheme } from '../../hooks/useTheme'
+import { cn } from '../../lib/utils'
 
 const NAV = [
   {
